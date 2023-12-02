134c07688a27e7624863045d4896dd34e13c4884
n-haruhi <hr23nakanishi@gmail.com>
2023-12-02 14:46:33 +0000

    [Update]詳細ページの新規投稿フォーム修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
