3c19e532964f12087526a202739ad51437c0faea
n-haruhi <hr23nakanishi@gmail.com>
2023-12-04 08:50:56 +0000

    [Update]投稿詳細画面にいいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create #いいねを作成
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_to book_path(book)
   end
 
   def destroy #いいねを削除
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to book_path(book)
   end
 
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,5 +3,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  varidates :user_id, uniqueness: {scope: :book_id} #scopeで、user_idとbook_idのペアに対してすでに同じ値のペアがテーブルに保存されていないかを判定
+  validates :user_id, uniqueness: {scope: :book_id}
 end
