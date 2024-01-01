82d3765d900d10ee78cfa67d1da34178d010ae81
n-haruhi <hr23nakanishi@gmail.com>
2024-01-01 16:31:02 +0000

    [Update]アソシエーション設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,7 @@ class Favorite < ApplicationRecord
 
   belongs_to :user
   belongs_to :book
+  has_one :notification, as: :notifiable, dependent: :destroy
 
   validates :user_id, uniqueness: {scope: :book_id}
 end
